287927ac9d2872f731f202879910b867345a0241
sdfdsfsfdsfd <sdfdsfsfdsfd>
2023-03-03 15:08:02 +0700

    DEV

--- average.cpp
+++ average.cpp
@@ -1,1 +1,16 @@
-
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(int argc ,char * argv[]){
+	double sum = 0;
+	if(argc == 1) cout << "Please input numbers to find average.";
+	else {
+		cout << "---------------------------------\n";
+		for(int i = 1 ; i < argc ;i++){
+			sum += atof(argv[i]);
+		}
+		cout << "Average of " << argc-1 << " numbers = " << sum/(argc-1) << "\n";
+		cout << "---------------------------------";
+	}
+	
+}
